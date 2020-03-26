df7b893f67864d2e1425be3225644ddf80585447
a.isakov <a.isakov@skbkontur.ru>
2020-03-26 21:35:36 +0500

    [new] new models for new contracts

---
+++ Models/NewModels/All.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace NewModels1
+{
+    public class OutputUnitState
+    {
+        public Guid Id { get; set; }
+        public Guid PlayerId { get; set; }
+        public int Health { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 RotationInEulerAngle { get; set; }
+        public UnitType Type { get; set; }
+        public Dictionary<string, string> Requisites { get; set; }
+    }
+
+    public class InputUnitState
+    {
+        public Guid Id { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 RotationInEulerAngle { get; set; }
+        public Dictionary<string, string> Requisites { get; set; }
+        public UnitType Type { get; set; }
+    }
+
+
+    public enum UnitType
+    {
+        Warrior = 100,
+        Runner = 101,
+        Garage = 200,
+        Stall = 201,
+        Tower = 202,
+        Base = 300
+    }
+
+    public class Vector3
+    {
+
+    }
+
+    public class OutputPlayerModel
+    {
+        public Guid Id { get; set; }
+        public string Nickname { get; set; }
+        public PlayerResources Resources { get; set; }
+        public Dictionary<string, string> Requisites { get; set; }
+    }
+
+    public class InputPlayerModel
+    {
+        public PlayerResources Resources { get; set; }
+        public Dictionary<string, string> Requisites { get; set; }
+    }
+
+    public class EnterSessionRequest
+    {
+        public Guid SessionId { get; set; }
+        public string Nickname { get; set; }
+    }
+
+    public class PlayerResources
+    {
+        public int Money { get; set; }
+        public int Beer { get; set; }
+    }
+}
